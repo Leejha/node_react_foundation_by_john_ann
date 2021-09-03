ad652b2c068ada6b95191921eecb73fc1d5ac414
Leejha <jhl2619@naver.com>
2021-09-03 13:12:44 +0000

    feat : Add register page

---
+++ client/src/_actions/user_action.js
@@ -0,0 +1,35 @@
+import axios from 'axios';
+import {
+    LOGIN_USER,
+    REGISTER_USER
+} from './types';
+
+const apiUrl = 'https://5000-turquoise-rhinoceros-aapmw8m3.ws-us16.gitpod.io';
+
+export function loginUser(body) {
+    const reqUrl = apiUrl + '/api/users/login';
+    const request = axios.post(reqUrl, body)
+        .then(response => response.data)
+
+        // const getData = async() => {
+        //     const response = await axios.post("reqUrl")
+        // }
+    
+        // getData()
+
+    return { 
+        type : LOGIN_USER,
+        payload : request
+    }
+}
+
+export const registerUser = (dataToSubmit) => {
+    const reqUrl = apiUrl + '/api/users/register';
+    const request = axios.post(reqUrl, dataToSubmit)
+        .then( response => response.data)
+
+    return { 
+        type : REGISTER_USER,
+        payload : request
+    }
+} 
--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -9,8 +9,16 @@ function LandingPage() {
     
     }, [])
     return (
-        <div>
-            LandingPage
+        <div style = {{
+            display : 'flex', justifyContent: 'center', alignItems : 'center',
+            width : '100%', height : '100vh'
+        }}>
+            <h2>
+                시작 페이지
+            </h2>
+            {/* <button  onClick = >
+                로그아웃
+            </button> */}
         </div>
     )
 }
